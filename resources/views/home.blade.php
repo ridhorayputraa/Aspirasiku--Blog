71568acffc1551b8a5ec5f33c6588ca8e23351bb
ridhorayputraa <ridhorayputra@gmail.com>
2022-11-11 16:07:54 +0700

    done for navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 
 @section('container')
+<div class="container mt-12">
+<h1>Selamat Datang di Aspirasiku</h1>
+</div>
 <h1>Halaman Home</h1>
 <p>on development</p>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,16 +1,16 @@
-<nav class="navbar navbar-expand-lg bg-warning">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg bg-danger ">
+    <div class="container mt-2">
+      <a class="navbar-brand" href="/"><h3 class="text-white">Aspirasiku</h3></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Pricing</a>
-          <a class="nav-link disabled">Disabled</a>
-        </div>
+
+        <ul class="navbar-nav ms-auto ">
+            <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="#">Login</a>
+            </li>
+        </ul>
       </div>
     </div>
   </nav>
