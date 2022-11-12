a5c6164a5725a509c17ec59882be9da90f638a48
ridhorayputraa <ridhorayputra@gmail.com>
2022-11-12 17:02:39 +0700

    done adding section home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,22 @@
 
 
 @section('container')
-<div class="container mt-12">
-<h1>Selamat Datang di Aspirasiku</h1>
-</div>
-<h1>Halaman Home</h1>
-<p>on development</p>
+<section class="hero pt-5  section">
+    <div class="container ">
+        <div class="row ">
+            <div class="col align-self-center">
+                <h1 class="mb-4" >Selamat Datang di Aspirasiku</h1>
+                <p class="mb-5">Gabung Sekarang untuk menyalurkan aspirasi</p>
+                 <a href="/" class="text-decoration-none btn btn-primary">Log In</a>
+            </div>
+            <div class="col algin-self-center">
+                <img src="../img/loud.jpg" class="rounded-circle" width="500px" alt="aspiration logo">
+            </div>
+            </div>
+    </div>
+
+</section>
+
+
 @endsection
+
